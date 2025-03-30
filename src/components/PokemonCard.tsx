a92d76f624a3039dcd9a2e3ad8039133d2520a4b
yakup <yakup.senel33@gmail.com>
2025-03-30 18:11:25 +0200

    uses of props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,18 +1,5 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
-    //const pokemon = pokemonList[1];
-        return pokemon.imgSrc ? 
+function PokemonCard({ pokemon}) {
+            return pokemon.imgSrc ? 
         <figure>
             <img src={pokemon.imgSrc} alt={pokemon.name} />
             <figcaption>{pokemon.name}</figcaption>
