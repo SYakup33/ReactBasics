2a297f2455e4bff8b044d88dc0eee3a22e055811
yakup <yakup.senel33@gmail.com>
2025-04-02 15:42:51 +0200

    added interactive button with the using of state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
@@ -10,14 +11,25 @@ const pokemonList = [
   },
   {
     name: "mew",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
   },
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  // On récupère le pokemon qui a le meme nom que pokemonName. 
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
   return (
     <div>
       <MyTitle />
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemon}/>
+      <button type="button" onClick={() => setPokemonName(pokemonList[0].name)}>Bulbasaur</button>
+      <button type="button" onClick={() => setPokemonName(pokemonList[1].name)}>Mew</button>
     </div>
   );
 }
